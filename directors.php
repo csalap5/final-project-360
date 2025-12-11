e8105a0412383ece797c4d70b88717b05374d376
kpfarn <kevin.farnsworth.23@cnu.edu>
2025-12-11 12:46:43 -0500

    first last for directors

--- directors.php
+++ directors.php
@@ -25,6 +25,13 @@ Welcome to <em>uMovies</em>, your destination for information on <a href="movies
 
 <p>
 <?php
+function formatName($name) {
+    if (strpos($name, ',') === false) return $name;
+    $parts = explode(',', $name); 
+    $last  = trim($parts[0]);     
+    $first = trim($parts[1]);       
+    return $first . ' ' . $last;   
+}
 @$directorsdb = new mysqli('127.0.0.1','uMoviesUser','anonymous','uMovies');
 @$directorsdb->set_charset("utf8");
 
@@ -55,8 +62,8 @@ else {
             $row = $result->fetch_assoc();
             echo "<tr class=\"highlight\">";
             echo "<td>".($i+1)."</td>";
-            echo "<td><a href=\"director.php?name=".$row['name']."\">".$row['name']."</a></td>";
-            echo"<td>".$row['year']."</td>";
+            $formattedName = formatName($row['name']);
+            echo "<td><a href=\"director.php?name=".urlencode($row['name'])."\">".$formattedName."</a></td>";            echo"<td>".$row['year']."</td>";
             echo "</tr>\n";
         }
     }
